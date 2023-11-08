e5e541e0dcb1446458c88b60b132d6121b0dac4c
Valdoveste <caio.valdoveste@outlook.com.br>
2023-11-08 18:54:49 -0300

    Implementing About

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import PocketBase from 'pocketbase';
+import { Component, OnInit, inject } from '@angular/core';
+import { ExperienceService } from 'src/app/service/experience.service';
+import { environment } from 'src/environments/environment';
+
+const pb = new PocketBase(environment.baseApiUrl);
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
+  experiences!: any;
+  fileExperience!: any;
+  ExperienceService = inject(ExperienceService);
+
+  private async getExperience() {
+    try {
+      this.experiences = await this.ExperienceService.getAllExperience();
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  ngOnInit(): void {
+    this.getExperience();
+  }
 
 }

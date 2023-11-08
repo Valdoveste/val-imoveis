f42590b8546f647137b43b57d5c46e1024de5ac2
Valdoveste <caio.valdoveste@outlook.com.br>
2023-11-08 11:09:58 -0300

    new service

---
+++ src/app/service/experience.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ExperienceService {
+
+  constructor() { }
+
+  async getPropertie(): Promise<PropertieModel[]> {
+    const pb = new PocketBase(environment.baseApiUrl);
+    const records: PropertieModel[] = await pb.collection('Propertie').getFullList({
+      sort: '-created',
+    });
+    return records;
+  }
+}

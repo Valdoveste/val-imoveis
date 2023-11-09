8bef98dd71dae93538971bfb34f66c859c44f965
Valdoveste <caio.valdoveste@outlook.com.br>
2023-11-09 12:28:27 -0300

    New component imports and NgMask

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { PropertieComponent } from './components/propertie/propertie.component';
 import { AboutComponent } from './components/about/about.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { NgxMaskDirective, NgxMaskPipe, provideNgxMask } from 'ngx-mask';
+import { PropertiesComponent } from './components/properties/properties.component';
+import { WrapperComponent } from './components/wrapper/wrapper.component';
 
 @NgModule({
   declarations: [
@@ -18,13 +20,15 @@ import { NgxMaskDirective, NgxMaskPipe, provideNgxMask } from 'ngx-mask';
     HomeComponent,
     PropertieComponent,
     AboutComponent,
-    ContactComponent
+    ContactComponent,
+    PropertiesComponent,
+    WrapperComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgxMaskDirective,
-    NgxMaskPipe
+    NgxMaskPipe,
   ],
   providers: [provideNgxMask()],
   bootstrap: [AppComponent]

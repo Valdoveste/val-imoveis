d9e6f8f4edd4696aa9422c18f414172fc8772f88
Valdoveste <caio.valdoveste@outlook.com.br>
2023-11-09 12:27:17 -0300

    New routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PropertieComponent } from './components/propertie/propertie.component';
+import { WrapperComponent } from './components/wrapper/wrapper.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: WrapperComponent
+  },
+  {
+    path: 'home',
+    redirectTo: ''
+  },
+  {
+    path: 'imoveis',
+    redirectTo: ''
+  },
+  {
+    path: 'construtoras',
+    redirectTo: ''
+  },
+  {
+    path: 'contato',
+    redirectTo: ''
+  },
+  {
+    path: 'imovel-detalhes/: id',
+    component: PropertieComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule {
-
-  
-
- }
+export class AppRoutingModule { }

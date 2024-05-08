58f3bf0d3c618613d74e9fe1642614b0528f7536
Valdoveste <caio.valdoveste@outlook.com.br>
2024-05-07 23:48:16 -0300

    Adding the model to the requests

--- src/app/service/geocoding-api.service.ts
+++ src/app/service/geocoding-api.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+import { GeocodingModel } from '../models/geocoding';
 
 const GEOCODING_URL: String = environment.GEOCODING_URL;
 const GEOCODING_API_KEY: String = environment.GEOCODING_API_KEY;
@@ -12,8 +14,8 @@ export class GeocodingApiService {
 
   constructor(private http: HttpClient) { }
 
-  getLocationLatLng(streetOutput: String) {
-    return this.http.get(
+  getLocationLatLng(streetOutput: String): Observable<GeocodingModel> {
+    return this.http.get<GeocodingModel>(
       GEOCODING_URL + "address=" + streetOutput + "&key=" + GEOCODING_API_KEY
     )
   }

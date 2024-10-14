faa42c9007d8a1d6f41d3fcb24cc9246b73e658f
Valdoveste <caio.valdoveste@outlook.com.br>
2024-10-13 23:51:30 -0300

    Removing "conenction to pb

--- src/app/components/propertie/propertie.component.ts
+++ src/app/components/propertie/propertie.component.ts
@@ -165,16 +165,16 @@ export class PropertieComponent implements OnInit {
   }
 
   private async setMetadataForPropertie(propertie: PropertieModel) {
-    const pb = new PocketBase(environment.POCKETBASE_URL);
+    // const pb = new PocketBase(environment.POCKETBASE_URL);
 
-    const record = await pb.collection('Propertie').getOne(propertie.id);
+    // const record = await pb.collection('Propertie').getOne(propertie.id);
 
     this.titleService.setTitle((propertie.imovel + propertie.endereco_bairro));
     this.metadataService.updateTag({ name: 'description', content: propertie.desc_imovel_completa });
     this.metadataService.updateTag({ property: 'og:title', content: propertie.imovel });
     this.metadataService.updateTag({ property: 'og:description', content: propertie.desc_imovel_simple });
     this.metadataService.updateTag({ property: 'og:url', content: window.location.href });
-    this.metadataService.updateTag({ property: 'og:image', content: (environment.POCKETBASE_URL + "/api/files/" + propertie.collectionId + "/" + propertie.id + "/" + record['foto_principal']) });
+    // this.metadataService.updateTag({ property: 'og:image', content: (environment.POCKETBASE_URL + "/api/files/" + propertie.collectionId + "/" + propertie.id + "/" + record['foto_principal']) });
   }
 
   private getPropertie() {
